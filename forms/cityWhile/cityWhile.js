52282ac92b6c9e7962648e05ad6d142ade7c3c6a
Trey-Gleason <wfg15722@creighton.edu>
2021-03-14 21:28:45 -0500

    Initial commit

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,22 @@
+/*
+cityWhile = []
+let keepgoing = true
+
+while (keepgoing == true) {
+  moreCity = prompt('so you want to enter a city?')
+  if (moreCity == "yes") {
+  city = prompt('enter a city')
+  cityWhile.push(city)
+}
+  else {
+  keepgoing = false
+  }
+  }
+let count = 0
+while (cityWhile.length !== count) {
+  let cities = cityWhile[count]
+  console.log(cities.toLowerCase())
+  count++
+}
+
+*/
---
+++ forms/dogStore/dogStore.js
@@ -0,0 +1,18 @@
+let product = ["Crocheted Dog Toy", "Slow Feeder Bowl", "Pawz Grey Treat Jar", "Blue Sweet Dreams Thermal Pajamas", "Checker Chewy Vuiton Bowl", "Red Heart Sweater Dress", "Velvet Hair Bow - Sun Kissed", "Into the Wild Leash"]
+let price = [10, 14, 20, 30, 32, 27, 12, 27]
+
+let wishList = []
+let unit = []
+
+btnDisplay.onclick=function() {
+  for (i = 0; i <= price.length -1; i++) {
+    inptItems.push(`${product[i]} : ${price[i]}`)
+}
+    lbl.value = `${unit}`
+}
+
+btnAddToWishList.onclick=function() {
+  let itemProduct = inptItems.value
+  lblwishList.push(product)
+  list.value = `${lblwishList}`
+}
